2832d180d276704089851df5577e86ee16625cbc
brandon <76394133+CXZJ@users.noreply.github.com>
2023-09-22 23:26:41 +0700

    Update Week2_Asssignment.py

--- Week2/Assignment/Week2_Asssignment.py
+++ Week2/Assignment/Week2_Asssignment.py
@@ -1,12 +1,51 @@
-'''
-Assignment:
+print("1. Basic")
+print("2. BMI")
+calculatorChoice = input("which calculator would you like to use? (type the number)")
 
-- Create a math calculator program
-- It should:
-  1. be able to calculate non whole number digits.
-  2. takes in user input for the numbers.
-  3. is able to calculate using all basic arithmetic operations(addition, subtraction, multiplication, division, power) - you can also go the extra mile by including roots.
-  4. does not produce an error when user does not input a number.
 
-- (optional): Create another type of calculator inside the program (example: BMI calculator, financial calculators, etc)
-'''
+while True:
+  if calculatorChoice == "1":
+    print("Which operation would you like to use?")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
+
+    def add(x, y):
+      return x + y
+
+    def subtract(x, y):
+      return x - y
+
+    def multiply(x, y):
+      return x * y
+
+    def divide(x, y):
+      return x / y
+
+    choice = input("Enter choice(1/2/3/4): ")
+
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+
+    if choice == "1":
+        print(add(num1, num2))
+        break
+    elif choice == "2":
+        print(subtract(num1, num2))
+        break
+    elif choice == "3":
+        print(multiply(num1, num2))
+        break
+    elif choice == "4":
+        print(divide(num1, num2))
+        break
+  elif calculatorChoice == "2":
+    weight = float(input("Please input your weight. (in kg)"))
+    height = float(input("Please input your height. (in m)"))
+
+    print(weight / (height**2.0))
+    break
+  else:
+    print("please choose a number 1 or 2")
+    break
